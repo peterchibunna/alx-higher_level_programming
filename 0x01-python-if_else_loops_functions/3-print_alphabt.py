fccf4d08f2a5a36bdfceba63dc444fa18f868ed9
Peter Chibunna <peterchibunna@gmail.com>
2022-09-20 08:43:05 -0700

    fix #3

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("".join(["{}".format(chr(a)) for a in range(97, 123) if a not in [101, 113]]), end="")
+print("".join(["{}".format(chr(a))
+               for a in range(97, 123) if a not in [101, 113]]), end="")
