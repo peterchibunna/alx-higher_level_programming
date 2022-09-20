080edfc117212353f06e083f68de792dadd43924
Peter Chibunna <peterchibunna@gmail.com>
2022-09-20 09:00:52 -0700

    task #5 pycodestyle: long line

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print(", ".join(["{}".format(str(a).zfill(2)) for a in range(0, 99+1)]), end="\n")
+print(", ".join(["{}".format(str(a).zfill(2))
+                 for a in range(0, 99+1)]), end="\n")
