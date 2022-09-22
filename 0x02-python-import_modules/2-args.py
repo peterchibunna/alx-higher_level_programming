2fb5676185ff2ad21455bf95eb3f98a6812c16bd
Peter Chibunna <peterchibunna@gmail.com>
2022-09-22 10:25:34 -0700

    task #2, fix pycodestyle

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,8 @@
 if "__main__" == __name__:
     import sys
     length = len(sys.argv) - 1
-    print("{} argument{}{}".format(length, "" if length == 1 else "s", "." if length == 0 else ":"))
+    print("{} argument{}{}".format(length, ""
+          if length == 1 else "s", "." if length == 0 else ":"))
     for idx, i in enumerate(list(range(0, length + 1))):
         if idx == 0:
             continue
