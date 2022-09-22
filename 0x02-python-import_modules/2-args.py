dfb50ed05ed88244213d5f4ee03c2c03a8944ac1
Peter Chibunna <peterchibunna@gmail.com>
2022-09-22 10:24:17 -0700

    task #2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+if "__main__" == __name__:
+    import sys
+    length = len(sys.argv) - 1
+    print("{} argument{}{}".format(length, "" if length == 1 else "s", "." if length == 0 else ":"))
+    for idx, i in enumerate(list(range(0, length + 1))):
+        if idx == 0:
+            continue
+        print("{}: {}".format(idx, sys.argv[i]))
