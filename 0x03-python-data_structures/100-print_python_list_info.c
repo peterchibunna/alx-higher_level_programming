c3b16b1bd88278d42aaa264fea67e436cc27fa97
Peter Chibunna <peterchibunna@gmail.com>
2022-09-26 14:16:29 -0700

    task #100

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,27 @@
+#include "Python.h"
+
+/**
+* print_python_list_info - prints info about a python list
+* @o: the python object as param
+* Returns: Nothing
+*/
+void print_python_list_info(PyObject *o)
+{
+	PyListObject *list;
+	Py_ssize_t size, i;
+	PyObject *object;
+	struct _typeobject *type;
+
+	if (strcmp(o->ob_type->tp_name, "list") == 0)
+	{
+		list = (PyListObject *)o;
+		size = list->ob_base.ob_size;
+		printf("[*] Size of the Python List = %ld\n", size);
+		printf("[*] Allocated = %ld\n", list->allocated);
+		for (i = 0; i < size; i++)
+		{
+			object = list->ob_item[i];
+			printf("Element %ld: %s\n", i, object->ob_type->tp_name);
+		}
+	}
+}
