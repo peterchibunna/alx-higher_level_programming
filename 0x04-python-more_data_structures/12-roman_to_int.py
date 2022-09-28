c972d9c003bfbaf913171149c6acc36803af7f1a
Peter Chibunna <peterchibunna@gmail.com>
2022-09-28 13:24:22 -0700

    task #12.5

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(ss):
-    romans = {'I': 1, "V": 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    list_of_num = [romans[char] for char in ss]
-    list_of_num = [list_of_num[index] - 2 * list_of_num[index - 1]
-                   if list_of_num[index - 1] < list_of_num[index] and index != 0
-                   else list_of_num[index]
-                   for index in range(len(list_of_num))]
-    return sum(list_of_num)
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    sum = 0
+
+    for index, char in enumerate(ss):
+        current_num = romans[char]
+        last_num = romans[ss[index - 1]]
+
+        if index != 0 and last_num < current_num:
+            sum += current_num - 2 * last_num
+        else:
+            sum += current_num
+    return sum
