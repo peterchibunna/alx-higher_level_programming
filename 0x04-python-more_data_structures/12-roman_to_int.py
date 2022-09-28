4f4fcc48aaedebb23f9bb4ee02f3706f52979a72
Peter Chibunna <peterchibunna@gmail.com>
2022-09-28 13:18:24 -0700

    task #12.4

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,9 @@
 #!/usr/bin/python3
 def roman_to_int(ss):
-    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    total = 0
-    ss = ss.replace("IV", "IIII")
-    ss = ss.replace("IX", "VIIII")
-    ss = ss.replace("XL", "XXXX")
-    ss = ss.replace("XC", "LXXXX")
-    ss = ss.replace("CD", "CCCC")
-    ss = ss.replace("CM", "DCCCC")
-    # replace those ambiguous ones that go backward
-    # and forward to always forward
-    # ie. the forwards only
-    for idx, i in enumerate(ss):
-        total += romans[i]
-    return abs(total)
+    romans = {'I': 1, "V": 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_of_num = [romans[char] for char in ss]
+    list_of_num = [list_of_num[index] - 2 * list_of_num[index - 1]
+                   if list_of_num[index - 1] < list_of_num[index] and index != 0
+                   else list_of_num[index]
+                   for index in range(len(list_of_num))]
+    return sum(list_of_num)
