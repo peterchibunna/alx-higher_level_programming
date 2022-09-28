be7b8e68d9be0641b43e417265e27a3357284df7
Peter Chibunna <peterchibunna@gmail.com>
2022-09-28 13:48:17 -0700

    task #12.7

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 def roman_to_int(ss):
-    a, r = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500,
-            "M": 1000, "IV": -2, "IX": -2, "XL": -20, "XC": -20,
-            "CD": -200, "CM": -200}, 0
-    for d, e in a.items():
-        r += ss.count(d) * e
-    return r
+    romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+
+    total = 0
+    prev_value = 0
+    for letter in ss:
+        value = romans[letter]
+        total += value
+        if value > prev_value:
+            total -= 2 * prev_value
+            prev_value = value
+    return total
