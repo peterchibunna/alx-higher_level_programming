ee18ebf8ed4a63e7b6c809a4b4bc71c92909e79b
Peter Chibunna <peterchibunna@gmail.com>
2022-10-10 06:57:26 -0700

    task #0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-	total = 0
-	if my_list:
-		for i in range(x):
-			try:
-				print(my_list[i], end="")
-				total += 1
-			except IndexError:
-				pass
-	print()
-	return total
+    total = 0
+    if my_list:
+        for i in range(x):
+            try:
+                print(my_list[i], end="")
+            except IndexError:
+                print("")
+                return total
+            else:
+                total += 1
+    print("")
+    return total
