ff8da566ea164b817cf12db47be983832142eeb4
Peter Chibunna <peterchibunna@gmail.com>
2022-10-10 06:48:18 -0700

    Task #0.4

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+def get_length(x):
+	total = 0
+	if x:
+		for i in x:
+			total += 1
+	return total
+
 def safe_print_list(my_list=[], x=0):
-	y = 0
-        for i in my_lst:
-            y += 1
+	y = get_length(my_list)
 	printable_length = y if x > y else x
 	try:
 		for i in my_list[:printable_length]:
@@ -12,3 +17,10 @@ def safe_print_list(my_list=[], x=0):
 	except Exception:
 		pass
 
+
+def safe_print_integer(value):
+	try:
+		print("{:d}".format(value))
+		return True
+	except ValueError:
+		return False
