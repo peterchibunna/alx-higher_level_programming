f5c76be5b96099c5dd9e52dfeeaffcc8af8da2a5
Peter Chibunna <peterchibunna@gmail.com>
2022-10-10 10:22:37 -0700

    Task #8

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def safe_function(fct, *args):
+    import sys
+    try:
+        result = fct(*args)
+        return result
+    except Exception as error:
+        sys.stderr.write("Exception: {}\n".format(error))
+        return None
