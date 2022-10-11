e02b85bd4244ffdb4caa9453050cc9173035c676
Peter Chibunna <peterchibunna@gmail.com>
2022-10-11 05:58:29 -0700

    Task #1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Module: 1-square
+Defines a Square of size: size
+"""
+
+
+class Square(object):
+    """
+    This is a simple Square class
+    """
+    def __init__(self, size):
+        """
+        Initialize a sq. of size
+
+        Args:
+            size: Description of `size'
+        """
+        self.__size = size
