acb0b9d1f7e79b294dfe56ecc9f6eccfea3059e1
Peter Chibunna <peterchibunna@gmail.com>
2022-10-11 07:35:38 -0700

    Task #3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module: 1-square
+Defines a Square of size: size
+"""
+
+
+class Square(object):
+    """
+    This is a simple Square class
+    """
+    def __init__(self, size=0):
+        """
+        Initialize a sq. of size
+
+        Args:
+            size: Description of `size' default to 0
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        size = self.__size
+        return size * size
