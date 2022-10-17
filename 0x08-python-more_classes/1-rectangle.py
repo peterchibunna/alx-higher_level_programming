ea3dd8187b7230bf911c27a8581037998b1885f3
Peter Chibunna <peterchibunna@gmail.com>
2022-10-17 16:06:33 +0100

    task #1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -23,7 +23,7 @@ class Rectangle(object):
         elif value < 0:
             raise ValueError('width must be >= 0')
         else:
-            self.__width = value   
+            self.__width = value
 
     @property
     def height(self):
