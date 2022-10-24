717bbef5dd513b487baf82a0e3bbe3c5f6f1278b
Peter Chibunna <peterchibunna@gmail.com>
2022-10-24 14:05:38 +0100

    Task #7 with tests

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """
 Module: 4-base_geometry
-An empty BaseGeometry class
+A BaseGeometry class
 """
 
 
-class BaseGeometry():
-    """An empty BaseGeometry class"""
+class BaseGeometry:
+    """A BaseGeometry class with area"""
 
     def area(self):
+        """definition to calculate area"""
         raise Exception('area() is not implemented')
 
     def integer_validator(self, name, value):
+        """Validate the value passed as integer"""
         if type(value) is not int:
-            raise TypeError('{} must be an integer'.format(name))
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError('{} must be greater than 0'.format(name))
+            raise ValueError("{} must be greater than 0".format(name))
