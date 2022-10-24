bafe5fcaf4c6bfefae756fe09a9cc24008890df7
Peter Chibunna <peterchibunna@gmail.com>
2022-10-24 12:34:50 +0100

    Task #0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module: 0-lookup:
+list of available attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """list of available attributes and methods of an object"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module: 1-my_list
+MyList inherits from list"""
+
+
+class MyList(list):    
+    """that prints the list, but sorted (ascending sort)"""
+    
+    def print_sorted(self):
+        """prints the sorted elements of the list"""
+        print(sorted(list(self)))
