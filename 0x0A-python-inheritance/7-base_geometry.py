6b64a40f16be4e410c4a8e754e85c8231fe934a4
Peter Chibunna <peterchibunna@gmail.com>
2022-10-24 13:44:34 +0100

    Task #7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module: 4-base_geometry
+An empty BaseGeometry class
+"""
+
+
+class BaseGeometry():
+    """An empty BaseGeometry class"""
+
+    def area(self):
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        if type(value) is not int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
