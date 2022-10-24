ad4742da2cea75b5fec429124a887d66af89f93d
Peter Chibunna <peterchibunna@gmail.com>
2022-10-24 20:29:36 +0100

    Task #9

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
-
 """
 Module:
 8-rectangle
 """
 
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     """Rectangle class that inherits `BaseGeometry`"""
 
     def __init__(self, width, height):
-        """Initialize a rectangle of width and height"""
+        """Initialises a rectangle of width and height"""
+
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """
 module: 8-rectangle:
 with class BaseGeometry
 """
 
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
 class Rectangle(BaseGeometry):
     """Rectangle class that inherits BaseGeometry"""
 
     def __init__(self, width, height):
-        """Initializes the attributes: width and height"""
+        """Initialises the attributes: width and height"""
+
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
