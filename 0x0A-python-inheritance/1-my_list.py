d62ebe34ebf0fe293ef93e4c04d72ad25ad597ea
Peter Chibunna <peterchibunna@gmail.com>
2022-10-24 13:02:23 +0100

    Task #2

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,9 +4,9 @@ Module: 1-my_list
 MyList inherits from list"""
 
 
-class MyList(list):    
+class MyList(list):
     """that prints the list, but sorted (ascending sort)"""
-    
+
     def print_sorted(self):
         """prints the sorted elements of the list"""
         print(sorted(list(self)))
