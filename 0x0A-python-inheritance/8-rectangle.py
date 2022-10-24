774c788fa698b425f63982e90bc3c9d5a4173171
Peter Chibunna <peterchibunna@gmail.com>
2022-10-24 19:43:54 +0100

    Task #8

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+"""
+Module: 8-rectangle
+"""
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class that inherits from `BaseGeometry`"""
+
+    def __init__(self, width, height):
+        """initialize a rectangle of width and height"""
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
