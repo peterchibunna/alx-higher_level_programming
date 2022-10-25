84223a421b789d7c7fe395f6c54c28d97de7ad5e
Peter Chibunna <peterchibunna@gmail.com>
2022-10-25 22:47:24 +0100

    Task #13

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,13 +3,13 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
-	"""A function that inserts a line of text"""
-	with open(filename, 'r') as file:
-		lines = file.readlines()
-	with open(filename, 'w') as file:
-		for line in lines:
-			if search_string in line:
-				file.write(line)
-				file.write(new_string)
-			else:
-				file.write(line)
+    """A function that inserts a line of text"""
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    with open(filename, 'w') as file:
+        for line in lines:
+            if search_string in line:
+                file.write(line)
+                file.write(new_string)
+            else:
+                file.write(line)
