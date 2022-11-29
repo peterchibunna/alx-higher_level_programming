dfece052b8b9eff5bd5caba6d6e629017ea34dd7
Peter Chibunna <peterchibunna@gmail.com>
2022-11-29 16:06:42 +0100

    Task #15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,2 +1,2 @@
 -- Number by score
-SELECT `score`, COUNT(`score`) AS `number` FROM `second_table` GROUP BY `score`;
+SELECT `score`, COUNT(`score`) AS `number` FROM `second_table` GROUP BY `score` ORDER BY `score`;
