2edbd750a53fc5ef80030723e08916f36d02fada
Peter Chibunna <peterchibunna@gmail.com>
2022-11-29 16:29:24 +0100

    Task #17, #18

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,3 +1,3 @@
 -- Temperatures #0
 USE hbtn_0c_0;
-SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures` GROUP BY `city` ORDER BY `avg_temp`;
+SELECT `city`, AVG(`value`) AS `avg_temp` FROM `temperatures` GROUP BY `city` ORDER BY `avg_temp` DESC;
