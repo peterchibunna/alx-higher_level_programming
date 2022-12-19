d2d6426a71a0052216abdfb56bdcf2335f6cf7d8
Peter Chibunna <peterchibunna@gmail.com>
2022-12-19 23:43:02 +0100

    11, 13

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 const myArgs = process.argv.slice(2);
-let numbers = [];
+const numbers = [];
 
-for (let i = 0; i < myArgs.length; i++) {
-  numbers.push(parseInt(myArgs[i]));
-}
+// for (let i = 0; i < myArgs.length; i++) {
+//   numbers.push(parseInt(myArgs[i]));
+// }
 if (myArgs.length === 0 || myArgs.length === 1) {
   console.log(0);
 } else {
-  console.log(numbers.sort().reverse()[1]);
+  console.log(myArgs.sort().reverse()[1]);
 }
--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 
-exports.add = function add(x, y) {
+exports.add = function add (x, y) {
   return parseInt(x) + parseInt(y);
-}
+};
