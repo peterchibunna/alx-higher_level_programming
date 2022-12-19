540802e6ab63ce7b95c1894b995537b5d44b473a
Peter Chibunna <peterchibunna@gmail.com>
2022-12-19 23:34:53 +0100

    task #7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,7 +3,7 @@
 if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   console.log('Missing number of occurrences');
 } else {
-  let count = parseInt(process.argv[2]);
+  const count = parseInt(process.argv[2]);
   for (let i = 0; i < count; i++) {
     console.log('C is fun');
   }
