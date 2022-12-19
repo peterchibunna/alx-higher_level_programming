ffa0efec70ca53e019a30bb9fe60c9700b22136c
Peter Chibunna <peterchibunna@gmail.com>
2022-12-19 23:47:21 +0100

    11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
 
 const myArgs = process.argv.slice(2);
-const numbers = [];
+// const numbers = [];
 
 // for (let i = 0; i < myArgs.length; i++) {
 //   numbers.push(parseInt(myArgs[i]));
 // }
-if (myArgs.length === 0 || myArgs.length === 1) {
+if (myArgs.length <= 1) {
   console.log(0);
 } else {
-  console.log(myArgs.sort().reverse()[1]);
+  const args = myArgs.map(Number).sort((a, b) => a - b);
+  console.log(args.sort().reverse()[1]);
 }
