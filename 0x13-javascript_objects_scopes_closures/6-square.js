ba9033a8e8fbb39254553da9846db940de0dd049
Peter Chibunna <peterchibunna@gmail.com>
2022-12-20 20:51:32 +0100

    Task #6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
 
-const Rectangle = require('./4-rectangle');
+const Square2 = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
+class Square extends Square2 {
 
   charPrint (ch) {
     const symbol = ch !== undefined ? ch : 'X';
