25d2dae150118b4c9bc930ff720fab00583a7c25
Peter Chibunna <peterchibunna@gmail.com>
2022-12-20 09:33:51 +0100

    #11: Sort as number

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
 
 const myArgs = process.argv.slice(2);
-const numbers = [];
-
-for (let i = 0; i < myArgs.length; i++) {
-  numbers.push(Number(myArgs[i]));
-}
 if (myArgs.length <= 1) {
   console.log(0);
 } else {
-  console.log(numbers.sort().reverse()[1]);
+  console.log(myArgs.map((item)=>Number(item)).sort((a, b) => a - b).reverse());
 }
