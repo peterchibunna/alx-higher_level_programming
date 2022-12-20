d273bea501b3ab305f555ebc9526d5ffc3bd41e5
Peter Chibunna <peterchibunna@gmail.com>
2022-12-20 21:08:23 +0100

    #8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,8 +2,8 @@
 
 exports.esrever = function (list) {
   const size = list.length;
-  let newList = Array();
-  for (let i = list.length - 1; i >= 0; i--) {
+  const newList = [];
+  for (let i = size - 1; i >= 0; i--) {
     newList.push(list[i]);
   }
   return newList;
