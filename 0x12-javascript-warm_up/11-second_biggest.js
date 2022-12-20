538f0c7f778b1fb848dc43801ecdf5a7faded912
Peter Chibunna <peterchibunna@gmail.com>
2022-12-20 09:36:55 +0100

    Task #11; fix sorting

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,9 @@
-#!/usr/bin/node
+#!/opt/homebrew/bin/node
+// #!/usr/bin/node
 
 const myArgs = process.argv.slice(2);
 if (myArgs.length <= 1) {
   console.log(0);
 } else {
-  console.log(myArgs.map((item)=>Number(item)).sort((a, b) => a - b).reverse());
+  console.log(myArgs.map((item) => Number(item)).sort((a, b) => a - b).reverse()[1]);
 }
