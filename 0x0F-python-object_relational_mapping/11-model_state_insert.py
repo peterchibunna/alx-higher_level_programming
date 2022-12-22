e30bfa07aacfc2abdd93e7a242aaa2ce29373382
Peter Chibunna <peterchibunna@gmail.com>
2022-12-22 21:41:42 +0100

    Task #11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""list all States from the database hbtn_0e_6_usa"""
+
+
+def main():
+    """connect to db, read data and display as expected using ORM"""
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    search = sys.argv[4]
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, database), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    louisiana = State(name='Louisiana')
+    session.add(louisiana)
+    session.commit()
+
+    added_data = session.query(State).filter(State.name == 'Louisiana').first()
+    print('{}'.format(added_data.id))
+    session.close()
+
+
+if __name__ == '__main__':
+    """execute the main function"""
+    main()
