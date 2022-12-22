683fcf3f8fdb18eee308d218d2582d3c1af0f59e
Peter Chibunna <peterchibunna@gmail.com>
2022-12-22 21:56:08 +0100

    Tasks #12, #13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""list all States from the database hbtn_0e_6_usa"""
+
+
+def main():
+    """connect to db, read data and display as expected using ORM"""
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, database), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    for state in session.query(State).filter(State.name.like('%a%')):
+        session.delete(state)
+    session.commit()
+    session.flush()
+    session.close()
+
+
+if __name__ == '__main__':
+    """execute the main function"""
+    main()
