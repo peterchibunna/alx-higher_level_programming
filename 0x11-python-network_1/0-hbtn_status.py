d40aee78845cb1b2cff38d11c99dc0ee468405c6
Peter Chibunna <peterchibunna@gmail.com>
2023-01-17 22:28:03 +0100

    #0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     with request.urlopen('https://intranet.hbtn.io/status') as connection:
         data = connection.read()
         print('Body response:')
-        print('    - type: {}'.format(type(data)))
-        print('    - content: {}'.format(data))
-        print('    - utf8 content: {}'.format(data.decode('utf-8', 'replace')))
+        print('\t- type: {}'.format(type(data)))
+        print('\t- content: {}'.format(data))
+        print('\t- utf8 content: {}'.format(data.decode('utf-8')))
